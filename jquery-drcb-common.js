8165bd6fd140cdd5cdb6fe4d6481673563fdc66b
CallMeMaster <315358257@qq.com>
2019-05-21 14:14:39 +0800

    add time interval
    新增定时调用，默认10秒。
    将外部传入方法名由trigger改为query

--- jquery-drcb-common.js
+++ jquery-drcb-common.js
@@ -1,8 +1,15 @@
 $.extend({
 	drcb: {
-		param:{column:[],description:"",trigger: function(){}},
+		dataIndex:0,
+		param: {
+			column: [],
+			description: "",
+			query: function () {},
+			time-inerval:10
+		},
 		initView: function (param) {
-			if(param==undefined||param==null) return;
+			if (param == undefined || param == null)
+				return;
 			this.param = param;
 			$("body").prepend("<div id='DRCBDiv' class='clickable' style='display:flex;flex-direction:row'></div>");
 			$("#DRCBDiv").append("<div style='margin:4 textAlign:center'>" + param.description + "</div>");
@@ -26,7 +33,7 @@ $.extend({
 						});
 					var datalist = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
 					$("#DRCBDiv").after("<div><table id='drcbGrid'><tr>" + table + "</tr><table><div>");
-					param.trigger(datalist);
+					starter(datalist);
 				};
 				reader.readAsBinaryString(f);
 			});
@@ -39,11 +46,41 @@ $.extend({
 			var tr = "";
 			$.each(data, function (i, item) {
 				tr += "<tr>";
-				$.each(this.param.column,function(j,column){
+				$.each(this.param.column, function (j, column) {
 					tr += "<td>" + item["column"] + "</td>";
 				});
 			});
 			$("#drcbGrid").append(tr);
+		},
+		
+		getTime = function(dataIndex){
+			return this.param.time-inerval;
+		},
+
+		starter = function (datalist) {
+			var run = function () {
+				console.log("search:"+datalist[dataIndex]);
+				var data = datalist[dataIndex];
+				//handle
+				try {
+					var result = this.param.query(data);
+					print(result);
+				} catch (e) {}
+				finally {
+					dataIndex++;
+				}
+				
+				clearInterval(runner);
+				runner = setInterval(run, getTime(dataIndex) * 1001);
+				if (dataIndex >= datalist.length) {
+					clearInterval(runner);
+					delete this.dataIndex;
+					//结束
+					$("body").replaceWith($("#dataGrid")[0].outerHTML);
+					console.log("done!");
+				}
+			}
+			runner = setInterval(run, getTime(dataIndex) * 1001);
 		}
 	}
 });
